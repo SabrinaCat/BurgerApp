a7e1082ea7e927d465f7de25aac80fa949c00e3e
SabrinaCat <Sabrina.glow22@gmail.com>
2020-07-24 20:53:54 -0500

    table schema and seeds tested and working

---
+++ database/schema.sql
@@ -0,0 +1,10 @@
+if exists drop database burger_db;
+create database burger_db;
+
+use burger_db
+
+create table burger_eats (
+    id int auto_increment not null primary key,
+    burger_name varchar (30) not null,
+    devoured boolean default 0
+)
