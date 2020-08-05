10c5cdec3b56c72ff98df2690d8e7bbac43c35b2
SabrinaCat <Sabrina.glow22@gmail.com>
2020-08-04 19:04:34 -0500

    finished routes

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -17,7 +17,25 @@ router.get("/", function(req, res) {
 });
 //this route will POST using our insertBurger function
 router.post("/api/burgers", function (req, res){
-  burger.createBurger(["burger_name"])
-})
+  burger.createBurger(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result){
+//return id of new burger
+    res.json({ id: result.insertId });
+  });
+});
+//update burger (how we change devoured from false to true)
+router.put("api/burgers/:id", function (req,res) {
+  let condition = "id = " + req.params.id;
+  //log the id
+  console.log("condition", condition);
+  burger.updateBurger({ devoured: req.body.devoured }, condition, function (result){
+  //if no burgers were changed then return error otherwise the response is 200 message
+    if (result.changedRows ===0) {
+      return res.status(404).end()
+    }
+    res.status(200).end();
+  });
+
+});
+module.exports = router;
 
 
