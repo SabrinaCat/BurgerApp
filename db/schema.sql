2aa407b26e5e37c7bc3353c0eb996106d4b86a24
SabrinaCat <Sabrina.glow22@gmail.com>
2020-08-20 18:01:31 -0500

    removed specific database name because now we are using jawsdb

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,8 @@
-if exists drop database burger_db;
-create database burger_db;
+--code before switching to heroku deploy/jawsdb
+-- if exists drop database burger_db;
+-- create database burger_db;
 
-use burger_db
+-- use burger_db
 
 create table burger_eats (
     id int auto_increment not null primary key,
